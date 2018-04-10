7e4480f684ee4d1f1d89636851ad96d40f55f0c5
dattechs <37616006+dattechs@users.noreply.github.com>
2018-04-10 10:03:07 +0200

    Create biblioteca.py

---
+++ addons/biblioteca/biblioteca.py
@@ -0,0 +1,32 @@
+  GNU nano 2.5.3                                                 File: biblioteca.py                                                                                                          
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 S&C (<http://salazarcarlos.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from opendattechs import models, fields, api, _
+
+class biblioteca_libro(models.Model):
+    _name = 'biblioteca.libro'
+
+    name = fields.Char(string='Titulo', required=True,)
+    description = fields.Text('Observacion')
+    date = fields.Date(string='Fecha de registro')
